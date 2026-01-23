5e9f0f854be7fbfc517d3b08c7efabf7ab1993a4
ssawaul <ssawaul@gmail.com>
2026-01-24 03:02:33 +0900

    초기 커밋.

---
+++ Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace keyviewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            panel2 = new Panel();
+            panel3 = new Panel();
+            panel4 = new Panel();
+            panel5 = new Panel();
+            panel6 = new Panel();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Location = new Point(81, 135);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(104, 96);
+            panel1.TabIndex = 0;
+            panel1.Paint += panel1_Paint;
+            // 
+            // panel2
+            // 
+            panel2.Location = new Point(191, 135);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(104, 96);
+            panel2.TabIndex = 1;
+            panel2.Paint += panel2_Paint;
+            // 
+            // panel3
+            // 
+            panel3.Location = new Point(301, 135);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(104, 96);
+            panel3.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            panel4.Location = new Point(411, 135);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(104, 96);
+            panel4.TabIndex = 3;
+            // 
+            // panel5
+            // 
+            panel5.Location = new Point(521, 135);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(104, 96);
+            panel5.TabIndex = 4;
+            // 
+            // panel6
+            // 
+            panel6.Location = new Point(631, 135);
+            panel6.Name = "panel6";
+            panel6.Size = new Size(104, 96);
+            panel6.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(panel6);
+            Controls.Add(panel5);
+            Controls.Add(panel4);
+            Controls.Add(panel3);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            KeyPreview = true;
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            KeyDown += Form1_KeyDown;
+            KeyUp += Form1_KeyUp;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private Panel panel3;
+        private Panel panel4;
+        private Panel panel5;
+        private Panel panel6;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,103 @@
+﻿namespace keyviewer
+{
+    public partial class Form1 : Form
+    {
+        private readonly Color[] _colors = { Color.Red, Color.Green, Color.Blue, Color.Yellow, Color.Purple, Color.Orange };
+        private int _colorIndex = 0;
+        private readonly Color _defaultColor = SystemColors.Control;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // 폴더/폰트 대화상자 이벤트(사용하지 않으면 그대로 둬도 됩니다)
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fontDialog1_Apply(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        // 키 누름: 누른 동안 색 변경
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            // 알파벳 키 매핑 예시
+            if (e.KeyCode == Keys.A)
+            {
+                panel1.BackColor = Color.Red;
+            }
+            else if (e.KeyCode == Keys.S)
+            {
+                panel2.BackColor = Color.Red;
+            }
+            else if (e.KeyCode == Keys.D)
+            {
+                panel3.BackColor = Color.Red;
+            }
+            else if (e.KeyCode == Keys.L)
+            {
+                panel4.BackColor = Color.Red;
+            }
+
+            // 세미콜론(;) — 대부분의 레이아웃에서 Keys.Oem1
+            else if (e.KeyCode == Keys.Oem1)
+            {
+                panel5.BackColor = Color.Red;
+            }
+
+            // 아포스트로피(') — 대부분의 레이아웃에서 Keys.Oem7
+            else if (e.KeyCode == Keys.Oem7)
+            {
+                panel6.BackColor = Color.Red;
+            }
+        }
+
+        // 키 뗌: 누르고 있던 키가 떼어지면 기본색으로 복원
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.A)
+            {
+                panel1.BackColor = _defaultColor;
+            }
+            else if (e.KeyCode == Keys.S)
+            {
+                panel2.BackColor = _defaultColor;
+            }
+            else if (e.KeyCode == Keys.D)
+            {
+                panel3.BackColor = _defaultColor;
+            }
+            else if (e.KeyCode == Keys.L)
+            {
+                panel4.BackColor = _defaultColor;
+            }
+            else if (e.KeyCode == Keys.Oem1)
+            {
+                panel5.BackColor = _defaultColor;
+            }
+            else if (e.KeyCode == Keys.Oem7)
+            {
+                panel6.BackColor = _defaultColor;
+            }
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
